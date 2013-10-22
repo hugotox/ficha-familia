0fe2285982ef5fd45e14abd9137965f2ffcf62e2
Hugo Pineda <hpineda@lawlogix.com>
2013-10-22 13:38:36 -0300

    mejorado performance in reporte datos parte 2

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django import forms
 from django.forms.widgets import DateInput
 from datetime import datetime
 import settings
+from utils.sql import get_dictfetchall_sql
 from utils.widgets import RutInput, HorizontalRadio
 
 PRESENTE_CHOICES = (
@@ -126,10 +127,31 @@ class CentroFamiliar(models.Model):
         familias = self.familia_set.all()
         cant_familias = familias.count()
         suma = 0.0
-        for fam in familias:
-            for estado in fam.estadofamiliaanio_set.filter(anio=anio):
-                if estado.porcentaje_datos_parte2:
-                    suma += estado.porcentaje_datos_parte2
+        # for fam in familias:
+        #     for estado in fam.estadofamiliaanio_set.filter(anio=anio):
+        #         if estado.porcentaje_datos_parte2:
+        #             suma += estado.porcentaje_datos_parte2
+        sql = '''
+            select
+              COM.id,
+              COM.comuna,
+              sum(EF.porcentaje_datos_parte2) as suma
+              from
+                main_centrofamiliar COM
+              inner join main_familia F on F.centro_familiar_id = COM.id
+              inner join main_estadofamiliaanio EF on EF.familia_id = F.id
+              where
+                EF.anio = %s
+                and COM.id = %s
+              group by
+                COM.id,
+                COM.comuna
+        '''
+        datos = get_dictfetchall_sql(sql, [anio, self.id])
+        if len(datos):
+            dato = datos[0]
+            suma = dato['suma']
+
         return suma / cant_familias
 
     def get_porcentaje_completo_p3(self):
