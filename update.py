4a900472caa90f568244f400814b47b2a318030a
Hugo Pineda <hpineda@lawlogix.com>
2013-12-17 11:00:57 -0300

    corregido update.py

--- update.py
+++ update.py
@@ -6,22 +6,9 @@ from main.models import *
 from django.db import connection, transaction
 
 cursor = connection.cursor()
-sql = '''
-    CREATE TABLE "main_estadocentroanio" (
-        "id" serial NOT NULL PRIMARY KEY,
-        "centro_id" integer NOT NULL REFERENCES "main_centrofamiliar" ("id") DEFERRABLE INITIALLY DEFERRED,
-        "anio" integer NOT NULL,
-        "porc_completo_parteI" double precision NOT NULL,
-        "porc_completo_parteII_ini" double precision NOT NULL,
-        "porc_completo_parteII_cum" double precision NOT NULL,
-        "porc_completo_parteIII" double precision NOT NULL,
-        UNIQUE ("centro_id", "anio")
-    );
-    CREATE INDEX "main_estadocentroanio_centro_id" ON "main_estadocentroanio" ("centro_id");
-'''
+sql = ""
 # cursor.execute(sql)
 # transaction.commit_unless_managed()
-
 
 anio = 2013
 
